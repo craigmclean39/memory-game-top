7d296e515088939c0d05a292b900d3761ce8b973
Craig McLean <craig.mclean39@gmail.com>
2021-10-07 11:26:19 -0700

    Add Logo to header

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,10 +1,20 @@
 import '../styles/mountainCard.css';
+import Mountain from '../media/mountain.png';
 
 const Scoreboard = (props) => {
   const { score, hiScore } = props;
   return (
     <div className='header'>
-      <h1 className='header--title'>Mountain Memory</h1>
+      <div className='header--title-container'>
+        <img
+          className='mountain-card--image'
+          src={Mountain}
+          alt='Mountain Memory'
+          width='80px'
+          height='80px'
+        />
+        <h1 className='header--title'>Mountain Memory</h1>
+      </div>
       <div className='header--scoreboard'>
         <p>Score: {score}</p>
         <p>Hi Score: {hiScore}</p>
